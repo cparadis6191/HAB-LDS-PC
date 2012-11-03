a6f8f2d7560be6bd654c6594965f6fd9667bb260
Colton Sablinsky <csablinsky@gmail.com>
2012-11-03 14:21:22 -0400

    All code completed, still need to debug once AVR software is done.

--- HAB-LDS-PC/src/PC.cpp
+++ HAB-LDS-PC/src/PC.cpp
@@ -3,14 +3,25 @@
 #include <string>
 #include <stdio.h>
 #include <stdlib.h>
+#include <fstream>
 
 using namespace std;
 
 
 int main(){
-	wstring serialPort;
+
+	char swtch='0';									//Switch for switch case menu
+	int poll=0;										//Used for setting polling interval
+
+	ofstream datafile;								//Datastream used for retrieving datafiles
+
+	char writeBuffer[1]={0},readBuffer[1]={0};		//Needed for writing and reading serially
+	DWORD BytesWrite=0,BytesRead=0;
+
+	wstring serialPort;								//Needed for opening a serial port
 	HANDLE hSerial;
-	
+		
+//Begin connection to serial port
 	wcout<<L"Enter which Serial Port to connect to: ";
 	wcin>>serialPort;
 	wstring prefix=L"\\\\.\\";
@@ -24,13 +35,13 @@ int main(){
 					   FILE_ATTRIBUTE_NORMAL,		//Sets file attributes (Set to standard)
 					   0);							//Ignored value since opening an existing file (Only for file creation)
 
-	if(hSerial==INVALID_HANDLE_VALUE){
+	if(hSerial==INVALID_HANDLE_VALUE){				//Error if an invalid serial port is input
 		if(GetLastError()==ERROR_FILE_NOT_FOUND){
 			cout<<"Specified Serial Port does not exist\n";
 			CloseHandle(hSerial);
 			exit(EXIT_FAILURE);
-		}
-		else{
+		}	
+		else{										//Error incase of unexpected errors
 			cout<<"Unspecifed Error:"<<GetLastError()<<endl;
 			CloseHandle(hSerial);
 			exit(EXIT_FAILURE);			
@@ -41,7 +52,7 @@ int main(){
 	
 	dcbSerial.DCBlength=sizeof(dcbSerial);			//Sets the length of the structure in bytes
 	
-	if(!GetCommState(hSerial,&dcbSerial)){
+	if(!GetCommState(hSerial,&dcbSerial)){			//Get current comm settings, send error if they can't be retrieved
 		cout<<"Error retrieving communication state\n";
 		CloseHandle(hSerial);
 		exit(EXIT_FAILURE);
@@ -52,48 +63,103 @@ int main(){
 	dcbSerial.StopBits=ONESTOPBIT;
 	dcbSerial.Parity=NOPARITY;
 	
-	if(!SetCommState(hSerial,&dcbSerial)){
+	if(!SetCommState(hSerial,&dcbSerial)){			//Set new comm settings, send error if they can't be set
 		cout<<"Error setting communication state.\n";
 		CloseHandle(hSerial);
 		exit(EXIT_FAILURE);
 	}
 	
-	COMMTIMEOUTS timeout={0};
-	
+	COMMTIMEOUTS timeout={0};						//Set so serial port timesout if no data is being sent
+													//Stops program from hanging while waiting for nonexistant data
 	timeout.ReadIntervalTimeout=50;
 	timeout.ReadTotalTimeoutConstant=50;
 	timeout.ReadTotalTimeoutMultiplier=10;
 	timeout.WriteTotalTimeoutConstant=50;
 	timeout.WriteTotalTimeoutMultiplier=10;
 	
-	if(!SetCommTimeouts(hSerial,&timeout)){
+	if(!SetCommTimeouts(hSerial,&timeout)){			//Set timeout settings, send error if can't be set
 		cout<<"Error setting timeout parameters.\n";
 		CloseHandle(hSerial);
 		exit(EXIT_FAILURE);
 	}
-	
-	
-	
-	/*char writeBuffer[1]={0};
-	writeBuffer[0]=0x56;
-	writeBuffer[1]='5';
-	DWORD BytesWrite=0;
+
+//Begin PC/AVR communication	
+	cout<<"Waiting to make connection\n";			//Sends the byte 0XAB to inform AVR connection is being attempted
+	writeBuffer[0]=0XAB;
 	if(!WriteFile(hSerial,writeBuffer,1,&BytesWrite,NULL)){
 		cout<<"Error occured while writing.\n";
 		CloseHandle(hSerial);
 	}
-	
-	char readBuffer[1]={0};
-	DWORD BytesRead=0;
-	
-	if(!ReadFile(hSerial,readBuffer,1,&BytesRead,NULL)){
-		cout<<"Error occured while reading.\n";
-		CloseHandle(hSerial);
-		exit(EXIT_FAILURE);
+	while(readBuffer[0]!=0xBB){						//Wait until byte 0XBB is recieved from AVR to know it is ready to commuicate
+		if(!ReadFile(hSerial,readBuffer,1,&BytesRead,NULL)){
+			cout<<"Error occured while reading.\n";
+			CloseHandle(hSerial);
+			exit(EXIT_FAILURE);
+		}	
 	}
-	cout<<readBuffer[0];*/
 	
-	CloseHandle(hSerial);
-	
-	return 0;
+//Begin setting polling or recieving data
+	while(1){
+													//Give switch-case options
+		cout<<"\nPress 'R' to begin recieving data from the device.\n";
+		cout<<"Press 'S' to begin sending a polling interval to the device.\n";
+		cout<<"Press 'E' to close out of the program.\n";
+		cin>>swtch;
+		
+		switch(swtch){
+		
+		case 'S':									//Give options for different polling intervals
+			cout<<"\nPress '1' to set polling interval to five seconds.\n";
+			cout<<"Press '2' to set polling interval to ten seconds.\n";
+			cout<<"Press '3' to set polling interval to fifteen seconds.\n";
+				   
+			cin>>poll;
+			if((poll>0)&&(poll<4)){					//Checks if polling interval is valid
+				writeBuffer[0]=0xEB;				//If valid send 0XEB to let AVR know a polling interval is coming
+				if(!WriteFile(hSerial,writeBuffer,1,&BytesWrite,NULL)){
+					cout<<"Error occured while writing.\n";
+					CloseHandle(hSerial);
+				}
+				writeBuffer[0]=poll*5;				//Followed by the actual polling interval
+				if(!WriteFile(hSerial,writeBuffer,1,&BytesWrite,NULL)){
+					cout<<"Error occured while writing.\n";
+					CloseHandle(hSerial);
+				}
+				cout<<"\nPolling interval has been sent.\n";
+			}
+			else{									//If input interval isn't valid prompt user
+				cout<<"Invalid input.";
+			}
+			break;
+		
+		case 'R':									//Begin reading in data
+			cout<<"\nReading in data, do not disconnect device.\n";
+			datafile.open("data.txt");				//Opens a file to write incoming data to
+			writeBuffer[0]=0xCB;					//Send 0XCB to AVR indicating that PC is wating for data
+			if(!WriteFile(hSerial,writeBuffer,1,&BytesWrite,NULL)){
+				cout<<"Error occured while writing.\n";
+				CloseHandle(hSerial);
+			}
+			while(readBuffer[0]!=0xDB){				//Read in data until 0XDB is sent indicating all data has been sent
+				if(!ReadFile(hSerial,readBuffer,1,&BytesRead,NULL)){
+					cout<<"Error occured while reading.\n";
+					CloseHandle(hSerial);
+					exit(EXIT_FAILURE);
+				}
+				datafile<<readBuffer[0];			//Write data to file byte by byte
+			}
+			datafile.close();						//Once all data has been read close file and prompt user.
+			cout<<"Data successfully writtien to \"data.txt\"";
+			break;
+		
+		case 'E':									//On quit close communication handle and return
+			CloseHandle(hSerial);
+			
+			return 0;
+		
+		default:									//On invalid input prompt user.
+			cout<<"\nInvalid input\n";
+			break;
+		}
+	}
 }
